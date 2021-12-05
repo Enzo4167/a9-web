a0ee6388ddc26a66933df1f5bed3edca4ef45045
enzo4167 <qi.l@northeastern.edu>
2021-12-05 00:36:21 -0800

    add who

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ require('./services/profile-service')(app);
 require('./lecture/tweet-service')(app);
 require('./services/movies-service')(app);
 require('./movies/service')(app);
+require('./services/who-service')（app);
 
 
 app.listen( process.env.PORT|| 4000);
